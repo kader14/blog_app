9aaaf3bbcbfbe569b9c0cc3a6c458a84919fa53b
Abdelkader Maarouf <dep2g2@gmail.com>
2023-10-23 19:57:21 +0200

    Changed database sqllite to mysql

--- my_blog/settings.py
+++ my_blog/settings.py
@@ -75,8 +75,18 @@ WSGI_APPLICATION = 'my_blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS':{
+            'init_command': 'SET default_storage_engine=INNODB',
+
+        },
+        'NAME': 'blogdb',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '3308',
     }
 }
 
